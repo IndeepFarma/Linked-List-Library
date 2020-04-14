e15aff76fe3097bb17cb949b93437188e0e4f87c
Indeep <indeepfarma@gmail.com>
2020-04-13 20:32:41 -0400

    Modulated it out more

--- linkedList.c
+++ linkedList.c
@@ -1,13 +1,7 @@
 /*  Author: Indeep Farma
     Linked List Library */
-#include <stdio.h>
-#include <stdlib.h>
 
-/* Create the linked list struct */
-typedef struct linkedListNode{
-  int data;
-  struct linkedListNode *next;
-} Node;
+#include "linkedList.h"
 
 /* Initialize a new Node */
 Node *initNewNode(int data)
@@ -20,26 +14,4 @@ Node *initNewNode(int data)
   newNode->next = NULL;
 
   return newNode;
-}
-
-int main(){
-
-  /* Declaring Variables */
-  Node *list = NULL;
-  Node *listNode = NULL;
-  // int i = 0;
-  int input = 0;
-
-  /* Malloc the array */
-  list = malloc(sizeof(Node));
-
-  /* Obtain input from the user */
-  printf("Enter number to add to list\n");
-  scanf("%d", &input);
-
-  listNode = initNewNode(input);
-
-  printf("List Node : %d\n", listNode->data);
-
-  return 0;
 }
